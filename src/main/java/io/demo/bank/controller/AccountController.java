7c3e3638a00822401f51ed0bc49db58b8a72ad3d
CDDAutomicDemoUser <64180134+CDDAutomicDemoUser@users.noreply.github.com>
2020-04-27 19:30:23 -0400

    US393 added comment

--- src/main/java/io/demo/bank/controller/AccountController.java
+++ src/main/java/io/demo/bank/controller/AccountController.java
@@ -45,8 +45,10 @@ import io.demo.bank.util.Patterns;
 public class AccountController extends CommonController {
 
 
-	private static final Logger LOG = LoggerFactory.getLogger(AccountController.class);         
+	private static final Logger LOG = LoggerFactory.getLogger(AccountController.class);
 	
+	/* test
+	*/
 	@Autowired
 	AccountService accountService;
 	

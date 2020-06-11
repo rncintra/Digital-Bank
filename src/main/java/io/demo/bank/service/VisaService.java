09198910ee18198c95511bf93743c725e350d415
AutomicDemo <36577894+AutomicDemo@users.noreply.github.com>
2020-06-11 17:17:31 +0200

    US440 Comments changed

--- src/main/java/io/demo/bank/service/VisaService.java
+++ src/main/java/io/demo/bank/service/VisaService.java
@@ -30,7 +30,7 @@ public class VisaService {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(VisaService.class);
 	
-	// Visa Service URL    
+	// Visa Service URL     
 	private static String apiBaseUrl;
 	
 	@Autowired

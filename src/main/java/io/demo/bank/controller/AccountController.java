cf54248a1be4c17d49477b30ed9351f66569f7d6
AutomicDemo <36577894+AutomicDemo@users.noreply.github.com>
2020-07-29 12:23:16 -0500

    US396

--- src/main/java/io/demo/bank/controller/AccountController.java
+++ src/main/java/io/demo/bank/controller/AccountController.java
@@ -53,7 +53,7 @@ public class AccountController extends CommonController {
 	
 	/*
 	 * ADMIN Role
-	 * Get All Accounts new release 20200723 2:43pm
+	 * Get All Accounts new release 20200723 2:43pm 
 	 */
 	@PreAuthorize(Constants.HAS_ROLE_ADMIN)
 	@GetMapping(Constants.URI_API_ACCT_ALL)

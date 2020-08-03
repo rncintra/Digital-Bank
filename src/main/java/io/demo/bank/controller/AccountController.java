01c40eb33c4949333fc9ac60d14d1ec04f859ae8
bkeefer55 <bkeefer55@gmail.com>
2020-08-03 16:37:05 +0100

    Update AccountController.java
    US408 - changing response

--- src/main/java/io/demo/bank/controller/AccountController.java
+++ src/main/java/io/demo/bank/controller/AccountController.java
@@ -50,7 +50,7 @@ public class AccountController extends CommonController {
 	@Autowired
 	AccountService accountService;
 	
-/* comment from bk  */	
+/* comment from bk1  */	
 	/*
 	 * ADMIN Role
 	 * Get All Accounts

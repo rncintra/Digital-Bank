4cb1ce28c27c10cac49dac46941a5ce248d19218
bkeefer55 <bkeefer55@gmail.com>
2020-08-04 10:33:25 +0100

    Update AccountController.java
    US408 - changing transfer method

--- src/main/java/io/demo/bank/controller/AccountController.java
+++ src/main/java/io/demo/bank/controller/AccountController.java
@@ -50,7 +50,7 @@ public class AccountController extends CommonController {
 	@Autowired
 	AccountService accountService;
 	
-/* comment from bk2  */	
+/* comment from bk3  */	
 	/*
 	 * ADMIN Role
 	 * Get All Accounts

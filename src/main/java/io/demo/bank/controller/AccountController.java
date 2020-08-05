eb1277ee83a33c186e260d9b16b41c58a3d435b9
bkeefer55 <bkeefer55@gmail.com>
2020-08-05 14:07:37 +0100

    US408 change admin role

--- src/main/java/io/demo/bank/controller/AccountController.java
+++ src/main/java/io/demo/bank/controller/AccountController.java
@@ -50,7 +50,7 @@ public class AccountController extends CommonController {
 	@Autowired
 	AccountService accountService;
 	
-/* comment from bk13  */	
+/* comment from bk14  */	
 	/*
 	 * ADMIN Role
 	 * Get All Accounts

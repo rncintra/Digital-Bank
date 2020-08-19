b25c8beb6b73f95a3a71919fcddffc2a064fca2e
tiagoms1 <tiago.ms1@gmail.com>
2020-08-19 19:06:16 -0300

    US396

--- src/main/java/io/demo/bank/controller/AccountController.java
+++ src/main/java/io/demo/bank/controller/AccountController.java
@@ -53,7 +53,7 @@ public class AccountController extends CommonController {
 	
 	/*
 	 * ADMIN Role
-	 * Get All Accounts new release 20200818 22:25pm 
+	 * Get All Accounts new release 20200819 07:07pm 
 	 */
 	@PreAuthorize(Constants.HAS_ROLE_ADMIN)
 	@GetMapping(Constants.URI_API_ACCT_ALL)

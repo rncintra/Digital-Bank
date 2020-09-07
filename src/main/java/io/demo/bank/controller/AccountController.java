4ef814f648315821568d647ed6ee740bb212e090
bkeefer55 <bkeefer55@gmail.com>
2020-09-07 14:03:36 +0100

    Update AccountController.java
    US465 comment from bk16

--- src/main/java/io/demo/bank/controller/AccountController.java
+++ src/main/java/io/demo/bank/controller/AccountController.java
@@ -50,7 +50,7 @@ public class AccountController extends CommonController {
 	@Autowired
 	AccountService accountService;
 	
-/* comment from bk15  */	
+/* comment from bk16  */	
 	/*
 	 * ADMIN Role
 	 * Get All Accounts

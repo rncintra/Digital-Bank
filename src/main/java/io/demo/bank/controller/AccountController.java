ec391ce669eff9f0db4dbd6bbd5ebb26e87953bc
bkeefer55 <bkeefer55@gmail.com>
2020-09-07 13:57:54 +0100

    US465 changing return value

--- src/main/java/io/demo/bank/controller/AccountController.java
+++ src/main/java/io/demo/bank/controller/AccountController.java
@@ -50,7 +50,7 @@ public class AccountController extends CommonController {
 	@Autowired
 	AccountService accountService;
 	
-/* comment from bk14  */	
+/* comment from bk15  */	
 	/*
 	 * ADMIN Role
 	 * Get All Accounts

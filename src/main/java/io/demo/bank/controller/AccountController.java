12fdd4d28be2d255c7c0f53d0f3b866990536d49
bkeefer55 <bkeefer55@gmail.com>
2020-09-16 13:02:50 +0100

    DE139876 correcting method name

--- src/main/java/io/demo/bank/controller/AccountController.java
+++ src/main/java/io/demo/bank/controller/AccountController.java
@@ -50,7 +50,7 @@ public class AccountController extends CommonController {
 	@Autowired
 	AccountService accountService;
 	
-/* comment from bk23  */	
+/* comment from bk24 */	
 	/*
 	 * ADMIN Role
 	 * Get All Accounts

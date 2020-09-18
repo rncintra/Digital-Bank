f6438bf68115e13f24afaa980055f99ee7b6d4a5
bkeefer55 <bkeefer55@gmail.com>
2020-09-18 15:03:31 +0100

    US932 change get account ownder

--- src/main/java/io/demo/bank/controller/AccountController.java
+++ src/main/java/io/demo/bank/controller/AccountController.java
@@ -50,7 +50,7 @@ public class AccountController extends CommonController {
 	@Autowired
 	AccountService accountService;
 	
-/* comment from bk32 */	
+/* comment from bk33 */	
 	/*
 	 * ADMIN Role
 	 * Get All Accounts

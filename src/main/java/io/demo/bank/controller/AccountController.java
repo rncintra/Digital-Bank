08c844b6cd7aef9bb9f44d8708000ef9f20d1770
garjo17 <39463300+garjo17@users.noreply.github.com>
2020-10-23 12:30:44 +0200

    Update AccountController.java

--- src/main/java/io/demo/bank/controller/AccountController.java
+++ src/main/java/io/demo/bank/controller/AccountController.java
@@ -52,7 +52,7 @@ public class AccountController extends CommonController {
 	
 	
 	/*
-	 * ADMIN Role
+	 * ADMIN Role  !! Hola cara de bola
 	 * Get All Accounts
 	 */ 
 	@PreAuthorize(Constants.HAS_ROLE_ADMIN)
